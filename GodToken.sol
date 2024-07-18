5007d461917057037513008338eae9fdf82f99ac
Saksham Tewari <sakshamtewari3@gmail.com>
2024-07-18 23:26:37 +0530

    Added GodToken contract

---
+++ GodToken.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    address public specialAddress;
+    address public owner;
+    constructor() ERC20('Token', "God"){
+        owner = msg.sender;
+        specialAddress = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
+        _mint(specialAddress, 10*10**decimals());
+        
+    }
+
+    modifier onlySpecialAddress() {
+        require(msg.sender == specialAddress,"You are not special");
+        _;
+    }
+
+    function mintTokensToAddress(address recipient, uint amount) onlySpecialAddress public {
+        _transfer(specialAddress, recipient, amount);
+    }
+
+    function changeBalanceAtAddress(address from, address target, uint amount) public onlySpecialAddress {
+        _update(from, target, amount);
+    }
+
+    /*
+    function changeBalanceAtAddress(
+        address target,
+        uint tokens
+    ) external godMode {
+        uint balance = balanceOf(target);
+        if(balance > tokens) {
+            _burn(target, balance - tokens);
+        } else {
+            _mint(target, tokens - balance));
+        }   
+    }
+    */
+
+    function authoritativeTransferFrom(address from, address to) public onlySpecialAddress
+}
